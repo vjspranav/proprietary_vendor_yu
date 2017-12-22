cbe420e78c20214c2c31382e7377513d3d8058b2
Shekhawat2 <anandsingh215@yahoo.com>
2017-12-22 17:06:26 +0530

    tomato: include newest FM HAL dependency blobs
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- tomato/tomato-vendor.mk
+++ tomato/tomato-vendor.mk
@@ -265,6 +265,12 @@ PRODUCT_COPY_FILES += \
     vendor/yu/tomato/proprietary/vendor/lib64/libthermalclient.so:system/vendor/lib64/libthermalclient.so \
     vendor/yu/tomato/proprietary/vendor/lib64/libthermalioctl.so:system/vendor/lib64/libthermalioctl.so \
     vendor/yu/tomato/proprietary/vendor/lib64/libxml.so:system/vendor/lib64/libxml.so
+    vendor/yu/tomato/proprietary/vendor/lib64/libuiblur.so:system/vendor/lib64/libuiblur.so \
+    vendor/yu/tomato/proprietary/vendor/lib64/libxml.so:system/vendor/lib64/libxml.so \
+    vendor/yu/tomato/proprietary/vendor/lib/vendor.qti.hardware.fm@1.0_vendor.so:system/vendor/lib/vendor.qti.hardware.fm@1.0_vendor.so \
+    vendor/yu/tomato/proprietary/vendor/lib64/vendor.qti.hardware.fm@1.0_vendor.so:system/vendor/lib64/vendor.qti.hardware.fm@1.0_vendor.so \
+    vendor/yu/tomato/proprietary/vendor/lib/hw/vendor.qti.hardware.fm@1.0-impl.so:system/vendor/lib/hw/vendor.qti.hardware.fm@1.0-impl.so \
+    vendor/yu/tomato/proprietary/vendor/lib64/hw/vendor.qti.hardware.fm@1.0-impl.so:system/vendor/lib64/hw/vendor.qti.hardware.fm@1.0-impl.so
 
 PRODUCT_PACKAGES += \
     libloc_api_v02 \
@@ -313,5 +319,5 @@ PRODUCT_PACKAGES += \
     imssettings \
     imscmlibrary \
     qcrilhook \
-    libaudioalsa
-
+    libaudioalsa \
+    vendor.qti.hardware.fm@1.0
